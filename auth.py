0f145cf44f99dffec2bb6451bcd4f4d0061a23cb
Mitali Singh <mitalisinghh07@gmail.com>
2025-07-25 05:56:34 +0530

    Added master password

--- auth.py
+++ auth.py
@@ -5,8 +5,8 @@ MASTER_PASSWORD = "passw@123"
 def verify_master_password():
     password = getpass.getpass("Enter master password: ")
     if password == MASTER_PASSWORD:
-        print("Access granted!")
+        print("✅ Access granted!")
         return True
     else:
-        print("Access denied! Wrong password.")
+        print("❌ Access denied! Wrong password.")
         return False

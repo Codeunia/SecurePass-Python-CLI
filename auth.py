a72b0c4f5dd5f70679697edfc88feef71315f19e
Mitali Singh <mitalisinghh07@gmail.com>
2025-07-25 05:47:46 +0530

    Added master password feature

---
+++ auth.py
@@ -0,0 +1,12 @@
+import getpass
+
+MASTER_PASSWORD = "passw@123"
+
+def verify_master_password():
+    password = getpass.getpass("Enter master password: ")
+    if password == MASTER_PASSWORD:
+        print("Access granted!")
+        return True
+    else:
+        print("Access denied! Wrong password.")
+        return False

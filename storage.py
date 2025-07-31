3acbd40bb48d7c905063be4f26b0701820f387ac
Mitali Singh <mitalisinghh07@gmail.com>
2025-07-31 22:51:49 +0530

    Encrypted storage for saved passwords

---
+++ storage.py
@@ -0,0 +1,31 @@
+import json
+import os
+from encryption import encrypt_password, decrypt_password
+
+DATA_FILE = "data.json"
+
+def load_data():
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, "r") as file:
+            return json.load(file)
+    return {}
+
+def save_data(data):
+    with open(DATA_FILE, "w") as file:
+        json.dump(data, file, indent=4)
+
+def save_password(service, username, password):
+    data = load_data()
+    data[service] = {
+        "username": username,
+        "password": encrypt_password(password)
+    }
+    save_data(data)
+
+def get_password(service):
+    data = load_data()
+    if service in data:
+        entry = data[service]
+        entry["password"] = decrypt_password(entry["password"])
+        return entry
+    return None
